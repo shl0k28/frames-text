8e4cc33d2a95556da038de1f6e8096b79ffeb2c3
Shlok <shlok_mange@protonmail.com>
2024-02-06 14:09:41 +0530

    migrate to framesjs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,41 @@
 import { Metadata } from "next";
-import Echo from "@/app/components/Echo";
+// import Echo from "@/app/components/Echo";
+import { useFramesReducer, FrameButton, FrameContainer, FrameImage, FrameInput, getPreviousFrame } from 'frames.js/next/server'
 
 const postUrl = `${process.env["HOST"]}/api/echo`;
 
-export async function generateMetadata(): Promise<Metadata> {
-  const imageUrl = `${process.env["HOST"]}/api/images/start?date=${Date.now()}`;
-  return {
-    title: "Echo the Dolphin",
-    description: "Type something and Echo will say it back!",
-    openGraph: {
-      title: "Echo the Dolphin",
-      images: [imageUrl],
-    },
-    other: {
-      "fc:frame": "vNext",
-      "fc:frame:image": 'http://localhost:3000/public/dark_imladris.png',
-      "fc:frame:post_url": postUrl,
-      // "fc:frame:input:text": "Type something here...",
-      "fc:frame:button:1": "Based Mint",
-    },
-  };
-}
+// export async function generateMetadata(): Promise<Metadata> {
+//   const imageUrl = `${process.env["HOST"]}/api/images/start?date=${Date.now()}`;
+//   return {
+//     title: "Echo the Dolphin",
+//     description: "Type something and Echo will say it back!",
+//     openGraph: {
+//       title: "Echo the Dolphin",
+//       images: [imageUrl],
+//     },
+//     other: {
+//       "fc:frame": "vNext",
+//       "fc:frame:image": 'http://localhost:3000/public/dark_imladris.png',
+//       "fc:frame:post_url": postUrl,
+//       // "fc:frame:input:text": "Type something here...",
+//       "fc:frame:button:1": "Based Mint",
+//     },
+//   };
+// }
+const reducer = (state: any, action: any) => ({ count: state.count + 1 });
 
-export default function Home() {
-  return (
-    <main className="flex flex-col text-center lg:p-16">
-      <Echo />
-    </main>
-  );
+export default async function Home(props: any){
+
+  const previousFrame = getPreviousFrame(props.searchParams)
+  const [state, dispatch] = useFramesReducer(reducer, { count: 0 }, previousFrame);
+
+  return <FrameContainer
+      postUrl="/frames"
+      state={state}
+      previousFrame={previousFrame}
+    >
+      <FrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
+      <FrameInput text="put some text here" />
+      <FrameButton onClick={dispatch}>Click me</FrameButton>
+    </FrameContainer>
 }
